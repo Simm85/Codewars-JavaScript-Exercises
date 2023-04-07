f0d7c6c2cf69b5a39732decbd96f827e7c2efb04
Simeon Petrov <simonpetrov85@gmail.com>
2023-04-07 20:10:56 +0300

    Create 04.growthOfPopulation.js

---
+++ Exercises/04.growthOfPopulation.js
@@ -0,0 +1,13 @@
+function nbYear(p0, percent, aug, p) {
+    let yearCount = 0;
+    percent /= 100;
+    while (p0 < p) {
+        yearCount++;
+        p0 += parseInt(p0 * percent) + aug;
+    }
+    return yearCount;
+}
+nbYear(1500, 5, 100, 5000);
+nbYear(1500000, 2.5, 10000, 2000000);
+nbYear(1500000, 0.25, 1000, 2000000);
+
