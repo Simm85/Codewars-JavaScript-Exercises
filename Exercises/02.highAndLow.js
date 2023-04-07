1b2e56731ed00e85566edeb5c88ff80605aba5c9
Simeon Petrov <simonpetrov85@gmail.com>
2023-04-07 13:42:01 +0300

    Update 02.highAndLow.js

--- Exercises/02.highAndLow.js
+++ Exercises/02.highAndLow.js
@@ -1,6 +1,7 @@
 function highAndLow(numbers) {
-    numbers = numbers.split(' ').map(number => Number(number));
-    if (numbers.length < 2) return new Array(numbers[0], numbers[0]).join(' ');
-    numbers.sort((a, b) => a - b);
-    return new Array(numbers[numbers.length - 1], numbers[0]).join(' ');
-  }
+  numbers = numbers.split(' ').map(Number);
+  if (numbers.length < 2) return new Array(numbers[0], numbers[0]).join(' ');
+  numbers.sort((a, b) => a - b);
+  return new Array(numbers.pop(), numbers.shift()).join(' ');
+}
+highAndLow('1 -3 5 105 20 49');
