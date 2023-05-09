e28c9f804e7160a63e7a06ac9e4dd50d11532a2e
Simeon Petrov <102179709+Simm85@users.noreply.github.com>
2023-05-09 19:58:06 +0300

    Create 26.sumStringsAsNumbers.js

---
+++ Exercises/26.sumStringsAsNumbers.js
@@ -0,0 +1,6 @@
+function sumStrings(a, b) {
+    return String(BigInt(a) + BigInt(b));
+}
+sumStrings('712569312664357328695151392', '8100824045303269669937');
+sumStrings('1', '2');
+sumStrings('123', '456'); // '579'
